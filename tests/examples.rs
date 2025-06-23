ced27523f4e14a684558069f5504b592ecc42f53
Jordan Hendl <jordiehendl@gmail.com>
2025-06-23 08:47:53 -0500

    chore: streamline examples

--- tests/examples.rs
+++ tests/examples.rs
@@ -3,12 +3,6 @@ use serial_test::serial;
 mod sample {
     include!("../examples/sample/bin.rs");
 }
-mod deferred_sample {
-    include!("../examples/sample_deferred/bin.rs");
-}
-mod shadow_sample {
-    include!("../examples/sample_shadows/bin.rs");
-}
 mod pbr_spheres {
     include!("../examples/pbr_spheres/bin.rs");
 }
@@ -18,20 +12,6 @@ mod pbr_spheres {
 #[ignore]
 fn run_sample() {
     sample::main();
-}
-
-#[test]
-#[serial]
-#[ignore]
-fn run_deferred_sample() {
-    deferred_sample::main();
-}
-
-#[test]
-#[serial]
-#[ignore]
-fn run_shadow_sample() {
-    shadow_sample::main();
 }
 
 #[test]
